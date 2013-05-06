bc9b11cdbca609de05578759b1d56612a3bc2cd5
Mohammad Reza Shariatzadeh <mrshcom@gmail.com>
2013-05-06 16:11:02 +0430

    add script file

---
+++ script.js
@@ -0,0 +1,13 @@
+
+(function($,undefined){
+
+  $(function(){
+    $('div.go2top').click(function(){
+      $('body').animate({
+          scrollTop: 0
+      }, 800);
+    });
+  })
+
+   
+})(jQuery);
